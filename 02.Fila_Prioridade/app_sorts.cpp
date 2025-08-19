66b4496f3949fd975eb164310cc95da9af7fff89
Marcelo Cohen <marcelo.cohen@pucrs.br>
2025-08-19 16:46:12 -0300

    Implementação de heapsort em C++, programa para teste de performance

--- 02.Fila_Prioridade/app_sorts.cpp
+++ 02.Fila_Prioridade/app_sorts.cpp
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <iostream>
+#include <cstdlib>
+#include <chrono>
 
 #include "mergesort.h"
 #include "quicksort.h"
@@ -8,20 +8,33 @@
 
 #define MAX 100000
 
+using namespace std;
+
 int main()
 {
     int data[MAX];
+    srand(time(NULL));
     for(int n=500; n<=MAX; n+=500) {
         for(int i=0; i<n; i++)
             data[i] = rand()%(MAX*10);
-        long start = clock();
         //MergeSort<int> ms;
         QuickSort<int> qs;
+        //MaxHeap<int> hs = MaxHeap(data, n);
+        auto start = std::chrono::high_resolution_clock::now();
         //ms.sort(data, n);
+        //hs.sort();
         qs.sort(data, n);
-        long end = clock();
-        float tempo = (end-start)/(float)CLOCKS_PER_SEC;
-        printf("%d %f\n", n, tempo);
+        //qsort(data, n, sizeof(int), compare); // usar com qsort
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float> duration = end - start;
+        int* vet = data;
+        //int* vet = hs.getArray(); // usar com heapsort
+        for(int i=0; i<n; i++)
+            cout << vet[i] << " ";
+        cout << endl;
+
+        cout << n << " " << setprecision(7) << fixed << duration.count() << endl;
+        break;
     }
 }
 
--- 02.Fila_Prioridade/maxheap.h
+++ 02.Fila_Prioridade/maxheap.h
@@ -14,21 +14,20 @@ private:
 
     void swim(int k) {
         while (k > 1 && heap[k / 2] < heap[k]) {
-            std::cout << "Swap " << heap[k] << " with " << heap[k / 2] << std::endl;
+            //std::cout << "Swap " << heap[k] << " with " << heap[k / 2] << std::endl;
             std::swap(heap[k], heap[k / 2]);
             k = k / 2;
         }
     }
 
-    void sink(int k) {
-        int size = heap.size() - 1;
+    void sink(int k, int size) {
         while (2 * k <= size) {
             int j = 2 * k;
             if (j < size && heap[j] < heap[j + 1])
                 j++;
             if (heap[k] >= heap[j])
                 break;
-            std::cout << "Swap " << heap[k] << " with " << heap[k / 2] << std::endl;
+            //std::cout << "Swap " << heap[k] << " with " << heap[k / 2] << std::endl;
             std::swap(heap[k], heap[j]);
             k = j;
         }
@@ -36,11 +35,15 @@ private:
 
 public:
     MaxHeap() {
-        heap.push_back(T()); // Placeholder for index 0
+        heap.push_back(T()); // Insere um elemento vazio no início
     }
 
-    // Crie um construtor que receba um vetor
-    // (para usar no sort)
+    // Construtor que recebe um array e copia para dentro do vector
+    // (não é o ideal mas garante a flexibilidade do tamanho variável
+    // do vector em situações "normais" - isto aqui é usado apenas no heapsort)
+    MaxHeap(T* entrada, int tam) {
+      heap.assign(entrada, entrada+tam);
+    }
 
     void put(T data) {
         heap.push_back(data);
@@ -49,16 +52,35 @@ public:
 
     T get() {
         if (heap.size() <= 1)
-            throw std::out_of_range("Heap is empty");
+            throw std::out_of_range("Heap vazio!");
         T res = heap[1];
         heap[1] = heap.back();
         heap.pop_back();
-        sink(1);
+        sink(1,heap.size());
         return res;
     }
 
+    // Retorna um ponteiro para o array interno gerenciado
+    // pelo vector (lembrando que esse ponteiro ficará inválido
+    // quando o vector deixar de existir)
+    T* getArray() {
+      return heap.data();
+    }
+
     void sort() {
-        // implemente heapsort!
+      int n = heap.size()-1;
+      for(int k=n/2; k>=1; k--) {
+        sink(k,n);
+      }
+
+      // 2. Ordena "removendo" o maior N-1 vezes (pois o último já vai estar na 1a. posição!)
+      while(n > 1) {
+        T tmp = heap[1];
+        heap[1] = heap[n];
+        heap[n] = tmp;
+        n--;
+        sink(1,n);
+      }
     }
 
     void print() const {
@@ -88,6 +110,13 @@ public:
             elem = 2 * elem;
             sp = sp / 2;
         }
+    }
+
+    void printArray() const {
+      for(int i=0; i<heap.size(); i++) {
+        std::cout << heap[i] << " ";
+      }
+      std::cout << std::endl;
     }
 };
 #endif
